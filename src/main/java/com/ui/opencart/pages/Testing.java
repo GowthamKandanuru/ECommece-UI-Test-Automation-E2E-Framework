33f716d75a5a92aa521f85d91ba61dd043006d26
GowthamKandanuru <50964789+GowthamKandanuru@users.noreply.github.com>
2025-12-15 15:58:59 +0530

    Add Testing class for WebDriver integration

---
+++ src/main/java/com/ui/opencart/pages/Testing.java
@@ -0,0 +1,13 @@
+package com.ui.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class Testing {
+
+    private WebDriver driver;
+
+    public Testing(WebDriver driver)
+    {
+        this.driver = driver;
+    }
+}
